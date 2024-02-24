208a00d42c803c45f1384d7dbc7d462e0a2995af
Nyasha Dubey <nyashadubey2003@gmail.com>
2024-02-24 15:16:17 +0530

    deploy

---
+++ Task1_fitness tracker/app.js
@@ -0,0 +1,9 @@
+// Handle active navigation link
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navLinks.forEach((otherLink) => otherLink.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
